c7f105c48a07c0ea01cd6ab9ed508f123ace2610
peychomilushev <f0rt3xx@gmail.com>
2019-03-16 09:27:33 +0200

    Presmyatane na litse na kvadrat

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proektw
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine(a * a);
+        }
+    }
+}
